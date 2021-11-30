8ac4f319ad57aa2713a90d11bc164ff0bf23b2d4
Peter Gonščák <peter.gonscak190@gmail.com>
2021-11-30 08:21:22 +0100

    done

--- Open-Lab-06.01/Challenge.cs
+++ Open-Lab-06.01/Challenge.cs
@@ -4,9 +4,22 @@ namespace Open_Lab_06._01
 {
     public class Challenge
     {
+        static public int a;
         public string MysteryFunc(string str)
         {
-            throw new NotImplementedException();
+            string s = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ("0123456789".Contains(str[i]))
+                {
+                    for (int x = 0; x < int.Parse(str[i].ToString()); x++) 
+                    { 
+                        s += str[(i - 1)]; 
+                    }
+                }
+            }
+            return s;
         }
     }
+    
 }
